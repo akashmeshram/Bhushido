74e219b095f4afcfcc8199c35193f45d6582a0da
akashmeshram <6388495akash@gmail.com>
2020-03-17 17:19:19 +0530

    Added link list template"

---
+++ EPI/8 link_list/linklist.cpp
@@ -0,0 +1,16 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node* next;
+
+int main()
+{   
+    Node* p = new Node(5);
+    cout << p->val;
+
+    return 0;
+}
